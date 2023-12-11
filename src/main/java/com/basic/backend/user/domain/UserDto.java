fdf65a82655679385f5ca0235b1664d4aafb61dd
reali23538 <awj0415@gmail.com>
2023-12-11 21:15:46 +0900

    swagger 설정

--- src/main/java/com/basic/backend/user/domain/UserDto.java
+++ src/main/java/com/basic/backend/user/domain/UserDto.java
@@ -1,11 +1,14 @@
 package com.basic.backend.user.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class UserDto {
 
+    @ApiModel(value = "사용자 등록 요청")
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
@@ -13,27 +16,35 @@ public class UserDto {
     @Setter
     public static class UserAddReq {
 
+        @ApiModelProperty(value="아이디", example = "reali23538", required = true)
         private String userId;
 
+        @ApiModelProperty(value="패스워드", required = true)
         private String password;
 
+        @ApiModelProperty(value="이름", example = "홍길동", required = true)
         private String name;
 
+        @ApiModelProperty(value="휴대폰", example = "111-2222-3333", required = true)
         private String phone;
 
+        @ApiModelProperty(value="이메일", example = "reali23538@gmail.com", required = true)
         private String email;
 
     }
 
+    @ApiModel(value = "사용자 등록 응답")
     @Builder
     @Getter
     @Setter
     public static class UserAddRes {
-
+        @ApiModelProperty(value="유저seq", example = "1", required = true)
         private Long userSeq;
 
+        @ApiModelProperty(value="아이디", example = "reali23538", required = true)
         private String userId;
 
+        @ApiModelProperty(value="패스워", required = true)
         private String password;
 
     }
--- src/main/java/com/basic/backend/user/web/UserController.java
+++ src/main/java/com/basic/backend/user/web/UserController.java
@@ -2,15 +2,18 @@ package com.basic.backend.user.web;
 
 import com.basic.backend.user.domain.UserDto;
 import com.basic.backend.user.service.UserService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
+@Api(tags = "사용자 API")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -24,27 +27,68 @@ public class UserController {
 //    @GetMapping("/profiles")
 //    public ResponseEntity<String> getProfiles() { return ResponseEntity.ok(profileName); }
 
+    @ApiOperation(
+            value = "사용자 등록",
+            notes = "사용자를 등록한다"
+    )
+    @ApiResponse(code = 201, message = "성공입니다")
     @PostMapping("/user")
     public ResponseEntity<UserDto.UserAddRes> add(@RequestBody UserDto.UserAddReq request) {
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(userService.add(request));
     }
 
+    @ApiOperation(
+            value = "사용자 리스트 조회",
+            notes = "사용자 리스트를 조회한다 (페이징)"
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name="page",
+            value = "요청 페이지",
+            dataType = "Integer",
+            paramType = "query", // query:@RequestParam, path:@PathVariable
+            defaultValue = "0"
+        ),
+        @ApiImplicitParam(
+            name="size",
+            value = "row 개수",
+            dataType = "Integer",
+            paramType = "query",
+            defaultValue = "10"
+        )
+    })
+    @ApiResponse(code = 200, message = "성공입니다")
     @GetMapping("/users")
-    public ResponseEntity<List<UserDto.UserRes>> getUsers(@PageableDefault(page = 1) Pageable pageable) {
+    public ResponseEntity<List<UserDto.UserRes>> getUsers(@ApiIgnore @PageableDefault(page = 1) Pageable pageable) {
         return ResponseEntity.ok(userService.getUsers(pageable).getContent());
     }
 
+    @ApiOperation(
+            value = "사용자 조회",
+            notes = "사용자를 조회한다"
+    )
+    @ApiResponse(code = 200, message = "성공입니다")
     @GetMapping("/users/{userSeq}")
     public ResponseEntity<UserDto.UserRes> getUser(@PathVariable(name = "userSeq") Long userSeq) throws Exception {
         return ResponseEntity.ok(userService.getUser(userSeq));
     }
 
+    @ApiOperation(
+            value = "사용자 수정",
+            notes = "사용자를 수정한다"
+    )
+    @ApiResponse(code = 200, message = "성공입니다")
     @PutMapping("/user")
     public ResponseEntity<UserDto.UserRes> update(@RequestBody UserDto.UserUdtReq userUdtReq) {
         return ResponseEntity.ok(userService.update(userUdtReq));
     }
 
+    @ApiOperation(
+            value = "사용자 삭제",
+            notes = "사용자를 삭제한다(유저 ID로)"
+    )
+    @ApiResponse(code = 200, message = "성공입니다")
     @DeleteMapping("/users/{userId}")
     public ResponseEntity<?> remove(@PathVariable(name = "userId") String userId) {
         userService.remove(userId);
